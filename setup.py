d389719921060ac2b1252e1ba5b1027d7c93a043
Ben Smith <ben@bensresearch.com>
2019-10-30 13:06:41 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='RankBasedMonteCarlo',
+    version='0.1dev',
+    packages=['RankBasedMonteCarlo',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    author='Ben Smith',
+    author_email='bosmith@unomaha.edu'
+)
