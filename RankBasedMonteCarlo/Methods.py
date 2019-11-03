bd6808bf3a73da650c13aa55cc00626b3843fc77
Ben <bosmith@unomaha.edu>
2019-11-03 14:43:37 -0600

    Cleanup Methods

--- RankBasedMonteCarlo/Methods.py
+++ RankBasedMonteCarlo/Methods.py
@@ -3,32 +3,46 @@ import pandas
 import sys
 import multiprocessing
 
+
 class _RankBasedMonteCarlo:
-	
-	def _RandomRank(self, ns):
-		objects = numpy.concatenate([numpy.repeat((i+1), n) for i, n in enumerate(ns)],axis=None)
-		numpy.random.shuffle(objects)
-		return pandas.DataFrame({'Group':objects.tolist(),}).assign(Rank = range(1,objects.size+1))
+    def _RandomRank(self, ns):
+        objects = numpy.concatenate(
+            [numpy.repeat((i + 1), n) for i, n in enumerate(ns)], axis=None
+        )
+        numpy.random.shuffle(objects)
+        return pandas.DataFrame({"Group": objects.tolist(),}).assign(
+            Rank=range(1, objects.size + 1)
+        )
 
-	def _RandomStatistic(self, sampleSizes):
-		sample = self._RandomRank(sampleSizes)
-		return self._CalculateStatistic(sample)
+    def _RandomStatistic(self, sampleSizes):
+        sample = self._RandomRank(sampleSizes)
+        return self._CalculateStatistic(sample)
 
-	def _PoolMonteCarlo(self, ns, reps):
-		p = multiprocessing.Pool()
-		r = []
-		for i, result in enumerate(p.imap_unordered(self._RandomStatistic, (ns,)*reps)):
-			r.append(result)
-			sys.stderr.write('\r{: .2f}% done'.format(100*i/reps))
-		p.close()
-		p.join()
-		return numpy.array(r)
+    def _PoolMonteCarlo(self, ns, reps):
+        p = multiprocessing.Pool()
+        r = []
+        for i, result in enumerate(
+            p.imap_unordered(self._RandomStatistic, (ns,) * reps)
+        ):
+            r.append(result)
+            sys.stderr.write("\r{: .2f}% done".format(100 * i / reps))
+        p.close()
+        p.join()
+        return numpy.array(r)
 
-	def _CalculateStatistic(self, sample):
-		return sample['Rank'].median()
+    def _CalculateStatistic(self, sample):
+        return sample["Rank"].median()
 
-	def PrintCriticalValueTable(self, ns, reps = 10000, observedValue = None, PrintToScreen = True, cvs = [0.01, 0.025, 0.05, 0.1], reverseDist = False):
-		""" 
+    def PrintCriticalValueTable(
+        self,
+        ns,
+        reps=10000,
+        observedValue=None,
+        PrintToScreen=True,
+        cvs=[0.01, 0.025, 0.05, 0.1],
+        reverseDist=False,
+    ):
+        """ 
 			Prints the critical values and p-value, if an observed test statistic is supplied.  The method returns a dictionary of critical values and p-value as a tuple.  
 		
 			Parameters
@@ -53,38 +67,73 @@ class _RankBasedMonteCarlo:
 			pvalue : float or None
 				The p-value of the observedValue specified in the parameter list (None if unspecified).
 		"""
-		r = self._PoolMonteCarlo(ns, reps)
+        r = self._PoolMonteCarlo(ns, reps)
 
-		cvresults = []
-		for i in cvs:
-			qvalue = numpy.quantile(r, i)
-			cvresults.append(qvalue)
-		
-		if PrintToScreen: print("\n" + pandas.DataFrame(zip(cvs, cvresults), columns=["Quantile", "Critical Value"]).to_string(index=False)) 
-		
-		try:
-			observedValue = float(observedValue)
-			pvalue = len(r[(r > observedValue)])/reps if reverseDist else len(r[(r < observedValue)])/reps
-			if PrintToScreen: print("Percent of distribution " + ("above" if reverseDist else "below") + " the observed value:" + "{: .4f}".format(pvalue))
-			return (dict(zip(cvs, cvresults)), pvalue)
-		
-		except:
-			return (dict(zip(cvs, cvresults)), None)
+        cvresults = []
+        for i in cvs:
+            qvalue = numpy.quantile(r, i)
+            cvresults.append(qvalue)
+
+        if PrintToScreen:
+            print(
+                "\n"
+                + pandas.DataFrame(
+                    zip(cvs, cvresults), columns=["Quantile", "Critical Value"]
+                ).to_string(index=False)
+            )
+
+        try:
+            observedValue = float(observedValue)
+            pvalue = (
+                len(r[(r > observedValue)]) / reps
+                if reverseDist
+                else len(r[(r < observedValue)]) / reps
+            )
+            if PrintToScreen:
+                print(
+                    "Percent of distribution "
+                    + ("above" if reverseDist else "below")
+                    + " the observed value:"
+                    + "{: .4f}".format(pvalue)
+                )
+            return (dict(zip(cvs, cvresults)), pvalue)
+
+        except:
+            return (dict(zip(cvs, cvresults)), None)
 
 
 class MonteCarloKolmogorovSmirnov(_RankBasedMonteCarlo):
-	""" Calculates the null hypothesis distribution of the two sample Kolmogorov-Smirnov test 
+    """ Calculates the null hypothesis distribution of the two sample Kolmogorov-Smirnov test 
 
 	"""
 
-	def _CalculateStatistic(self, sample):
-		n1len = len(sample[sample['Group'] == 1].index)
-		n2len = len(sample[sample['Group'] == 2].index)
-		return numpy.array([numpy.abs(len(sample[((sample['Group'] == 1) & (sample['Rank'] <= x))].index)/n1len -
-                    len(sample[((sample['Group'] == 2) & (sample['Rank'] <= x))].index)/n2len) for x in sample['Rank']]).max()
-	
-	def PrintCriticalValueTable(self, ns, reps = 10000, observedValue = None, PrintToScreen = True, cvs = [0.9, 0.95, 0.975, 0.99], reverseDist = True):
-		""" 
+    def _CalculateStatistic(self, sample):
+        n1len = len(sample[sample["Group"] == 1].index)
+        n2len = len(sample[sample["Group"] == 2].index)
+        return numpy.array(
+            [
+                numpy.abs(
+                    len(sample[((sample["Group"] == 1) & (sample["Rank"] <= x))].index)
+                    / n1len
+                    - len(
+                        sample[((sample["Group"] == 2) & (sample["Rank"] <= x))].index
+                    )
+                    / n2len
+                )
+                for x in sample["Rank"]
+            ]
+        ).max()
+
+    def PrintCriticalValueTable(
+        self,
+        ns,
+        reps=10000,
+        observedValue=None,
+        PrintToScreen=True,
+        cvs=[0.9, 0.95, 0.975, 0.99],
+        reverseDist=True,
+    ):
+        """ 
 			Prints the critical values and p-value, if an observed test statistic is supplied.  The method returns a dictionary of critical values and p-value as a tuple.  
 		
 			Parameters
@@ -109,25 +158,49 @@ class MonteCarloKolmogorovSmirnov(_RankBasedMonteCarlo):
 			pvalue : float or None
 				The p-value of the observedValue specified in the parameter list (None if unspecified).
 		"""
-		return super().PrintCriticalValueTable(ns, reps, observedValue, PrintToScreen, cvs, reverseDist)
+        return super().PrintCriticalValueTable(
+            ns, reps, observedValue, PrintToScreen, cvs, reverseDist
+        )
 
 
 class MonteCarloKuiper(_RankBasedMonteCarlo):
-	""" Calculates the null hypothesis distribution of the two sample Kuiper test 
+    """ Calculates the null hypothesis distribution of the two sample Kuiper test 
 
 	"""
 
-	def _CalculateStatistic(self, sample):
-		n1len = len(sample[sample['Group'] == 1].index)
-		n2len = len(sample[sample['Group'] == 2].index)
-		dPos = numpy.array([len(sample[((sample['Group'] == 1) & (sample['Rank'] <= x))].index)/n1len -
-                   len(sample[((sample['Group'] == 2) & (sample['Rank'] <= x))].index)/n2len for x in sample['Rank']]).max()
-		dNeg = numpy.array([len(sample[((sample['Group'] == 2) & (sample['Rank'] <= x))].index)/n2len -
-                   len(sample[((sample['Group'] == 1) & (sample['Rank'] <= x))].index)/n1len for x in sample['Rank']]).max()
-		return dPos + dNeg
-	
-	def PrintCriticalValueTable(self, ns, reps = 10000, observedValue = None, PrintToScreen = True, cvs = [0.9, 0.95, 0.975, 0.99], reverseDist = True):
-		""" 
+    def _CalculateStatistic(self, sample):
+        n1len = len(sample[sample["Group"] == 1].index)
+        n2len = len(sample[sample["Group"] == 2].index)
+        dPos = numpy.array(
+            [
+                len(sample[((sample["Group"] == 1) & (sample["Rank"] <= x))].index)
+                / n1len
+                - len(sample[((sample["Group"] == 2) & (sample["Rank"] <= x))].index)
+                / n2len
+                for x in sample["Rank"]
+            ]
+        ).max()
+        dNeg = numpy.array(
+            [
+                len(sample[((sample["Group"] == 2) & (sample["Rank"] <= x))].index)
+                / n2len
+                - len(sample[((sample["Group"] == 1) & (sample["Rank"] <= x))].index)
+                / n1len
+                for x in sample["Rank"]
+            ]
+        ).max()
+        return dPos + dNeg
+
+    def PrintCriticalValueTable(
+        self,
+        ns,
+        reps=10000,
+        observedValue=None,
+        PrintToScreen=True,
+        cvs=[0.9, 0.95, 0.975, 0.99],
+        reverseDist=True,
+    ):
+        """ 
 			Prints the critical values and p-value, if an observed test statistic is supplied.  The method returns a dictionary of critical values and p-value as a tuple.  
 		
 			Parameters
@@ -153,39 +226,56 @@ class MonteCarloKuiper(_RankBasedMonteCarlo):
 				The p-value of the observedValue specified in the parameter list (None if unspecified).
 
 		"""
-		return super().PrintCriticalValueTable(ns, reps, observedValue, PrintToScreen, cvs, reverseDist)
+        return super().PrintCriticalValueTable(
+            ns, reps, observedValue, PrintToScreen, cvs, reverseDist
+        )
+
 
 class MonteCarloMannWhitney(_RankBasedMonteCarlo):
-	""" Calculates the null hypothesis distribution of the Mann-Whitney test 
+    """ Calculates the null hypothesis distribution of the Mann-Whitney test 
 
 	"""
 
-	def _CalculateStatistic(self, sample):
-		n1len = len(sample[sample['Group'] == 1].index)
-		U1 = sample[sample['Group'] == 1]['Rank'].sum() - (n1len*(n1len+1))/2
-		return U1
+    def _CalculateStatistic(self, sample):
+        n1len = len(sample[sample["Group"] == 1].index)
+        U1 = sample[sample["Group"] == 1]["Rank"].sum() - (n1len * (n1len + 1)) / 2
+        return U1
+
 
 class MonteCarloKruskalWallis(_RankBasedMonteCarlo):
-	""" Calculates the null hypothesis distribution of the Kruskal–Wallis test 
+    """ Calculates the null hypothesis distribution of the Kruskal–Wallis test 
 
 	"""
 
-	def _CalculateStatistic(self, sample):
-		n = len(sample.index)
-		rbar = 0.5*(n+1)
-		divisor = 0
-		numerator = 0
-		
-		for group in pandas.unique(sample['Group']):
-			grouplen = len(sample[sample['Group'] == group].index)
-			groupmean = sample[sample['Group'] == group]['Rank'].mean()
-			divisor += numpy.array([numpy.power((x-rbar),2) for x in sample[sample['Group'] == group]['Rank']]).sum()
-			numerator += grouplen*numpy.power((groupmean-rbar),2)
-		
-		return ((n-1)*numerator)/divisor
+    def _CalculateStatistic(self, sample):
+        n = len(sample.index)
+        rbar = 0.5 * (n + 1)
+        divisor = 0
+        numerator = 0
 
-	def PrintCriticalValueTable(self, ns, reps = 10000, observedValue = None, PrintToScreen = True, cvs = [0.9, 0.95, 0.975, 0.99], reverseDist = True):
-		""" 
+        for group in pandas.unique(sample["Group"]):
+            grouplen = len(sample[sample["Group"] == group].index)
+            groupmean = sample[sample["Group"] == group]["Rank"].mean()
+            divisor += numpy.array(
+                [
+                    numpy.power((x - rbar), 2)
+                    for x in sample[sample["Group"] == group]["Rank"]
+                ]
+            ).sum()
+            numerator += grouplen * numpy.power((groupmean - rbar), 2)
+
+        return ((n - 1) * numerator) / divisor
+
+    def PrintCriticalValueTable(
+        self,
+        ns,
+        reps=10000,
+        observedValue=None,
+        PrintToScreen=True,
+        cvs=[0.9, 0.95, 0.975, 0.99],
+        reverseDist=True,
+    ):
+        """ 
 			Prints the critical values and p-value, if an observed test statistic is supplied.  The method returns a dictionary of critical values and p-value as a tuple.  
 		
 			Parameters
@@ -210,5 +300,6 @@ class MonteCarloKruskalWallis(_RankBasedMonteCarlo):
 			pvalue : float or None
 				The p-value of the observedValue specified in the parameter list (None if unspecified).
 		"""
-		return super().PrintCriticalValueTable(ns, reps, observedValue, PrintToScreen, cvs, reverseDist)
-
+        return super().PrintCriticalValueTable(
+            ns, reps, observedValue, PrintToScreen, cvs, reverseDist
+        )
