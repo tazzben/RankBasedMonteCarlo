0226ab9c27614060e92b6eb4c882b453782271b3
Ben <bosmith@unomaha.edu>
2019-11-04 06:33:17 -0600

    Update tag version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RankBasedMonteCarlo',
-    version='0.1.2',
+    version='0.1.3',
     packages=['RankBasedMonteCarlo',],
     license='MIT',
     install_requires=[
@@ -22,6 +22,6 @@ setup(
     ],
     keywords = ['Monte Carlo', 'Rank based', 'Statistics'],
     url = 'https://github.com/tazzben/RankBasedMonteCarlo',
-    download_url = 'https://github.com/tazzben/RankBasedMonteCarlo/archive/v0.1.2.tar.gz',  
+    download_url = 'https://github.com/tazzben/RankBasedMonteCarlo/archive/v0.1.3.tar.gz',  
     description = 'A set of Monte Carlo based tools to create test statistics for four non-parametric rank-based tests: Mann-Whitney (MW), Kruskal-Wallis (KW), Kolmogorov-Smirnov (KS) and Kuiper (K).'
 )
